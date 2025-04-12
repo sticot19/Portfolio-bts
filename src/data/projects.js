89250b5df1fe7a25a3d9a4fcaf13aac6b0d02e84
sticot19 <dumond.camille@gmail.com>
2025-04-12 21:58:43 +0200

    ajout des documents projet

---
+++ src/data/projects.js
@@ -0,0 +1,16 @@
+const projects = [
+    {
+        title: "Web Radio",
+        description: "Projet de 1ère année centré sur le streaming audio et la mise en place d’un serveur web.",
+        content:"Rôle : chef de projet<br/>Objectif : Réalisation d'un serveur de diffusion de média dans le cadre d'un projet en groupe.<br/><ul><li>Installation de Mixx et Icecast sur une VM et mise en place d'un SFTP afin de transférer les musiques</li><li>Création d'un site web hébergé sur un 2eme VM</li><li>Créations de musiques via suno.ai pour limité les droit d'auteur</li></ul><br/>Difficultées : Récupérer les données des musiques pour le lecteur audio",
+        media: ["public/web-radio/1.png", "public/web-radio/2.png", "public/web-radio/3.png", "public/web-radio/4.png"]
+    },
+    {
+        title: "Projet Casamance",
+        description: "Préparation de environ 25 postes pour l'association <a href='https://solidarite-casamance.fr/' target='_blank' className='text-blue-500 underline'>Les amis de la Casamance</a>.",
+        content:"L'objectif était de préparer plus ou moins 25 postes. POur cela on as effectués plusieurs manipulation :<ul><li>Changement de pate thermique</li><li>Benchmark pour vérifier le bon fonctionnement de tout les composents</li><li>Installation d'un linux avec la distrib xubuntu incluant les logiciels de traitement de text libreoffice ainsi que firefox et VLC</li></ul>"
+    },
+  ];
+  
+  export default projects;
+  
